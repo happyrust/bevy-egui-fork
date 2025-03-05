a3007ba6369fc9ec2a41a6ecc14ec363a5362268
happyrust <happydpc@sina.com>
2025-03-05 11:31:28 +0800

    Refactor example code and update dependencies.
    Replaced `.single()` with `.single().unwrap()` for better error handling, commented out unused `PickingBehavior`, and adjusted vector initialization syntax for clarity. Fixed `bevy_picking` dependency path in `Cargo.toml` to ensure consistency.

--- examples/render_to_image_widget.rs
+++ examples/render_to_image_widget.rs
@@ -147,7 +147,7 @@ fn render_to_image_example_system(
     mut contexts: EguiContexts,
 ) {
     let cube_preview_texture_id = contexts.image_id(&cube_preview_image).unwrap();
-    let preview_material_handle = preview_cube_query.single();
+    let preview_material_handle = preview_cube_query.single().unwrap();
     let preview_material = materials.get_mut(preview_material_handle).unwrap();
 
     let ctx = contexts.ctx_mut();
@@ -187,7 +187,7 @@ fn render_to_image_example_system(
     if apply {
         let material_clone = preview_material.clone();
 
-        let main_material_handle = main_cube_query.single();
+        let main_material_handle = main_cube_query.single().unwrap();
         materials.insert(main_material_handle, material_clone);
     }
 }
